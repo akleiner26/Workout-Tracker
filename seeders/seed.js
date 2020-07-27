10b8f30c837e182e8d36a655bb5384a289ee5b5e
orangeblack13 <AndrewJKleiner@gmail.com>
2020-07-27 15:35:38 -0400

    seeds rearranged and functioning

--- seeders/seed.js
+++ seeders/seed.js
@@ -116,54 +116,63 @@ let exerciseSeed = [
   }
 ]
 
-let workoutSeed = [
-  {
-    day: new Date().setDate(new Date().getDate()-10),
-    
-  },
-  {
-    day: new Date().setDate(new Date().getDate()-9),
-  },
-  {
-    day: new Date().setDate(new Date().getDate()-8),
-    
-  },
-  {
-    day: new Date().setDate(new Date().getDate()-7),
-    
-  },
-  {
-    day: new Date().setDate(new Date().getDate()-6),
-    
-  },
-  {
-    day: new Date().setDate(new Date().getDate()-5),
-    
-  },
-  {
-    day: new Date().setDate(new Date().getDate()-4),
-    
-  },
-  {
-    day: new Date().setDate(new Date().getDate()-3),
-    
-  },
-  {
-    day: new Date().setDate(new Date().getDate()-2),
-    
-  },
-  {
-    day: new Date().setDate(new Date().getDate()-1), 
-  }
-];
-
-db.Workout.deleteMany({})
-  .then(() => db.Workout.collection.insertMany(workoutSeed))
-  db.Exercise.collection.insertMany(exerciseSeed)
+db.Exercise.deleteMany({})
+  .then(() => db.Exercise.collection.insertMany(exerciseSeed))
   .then(data => {
-    console.log(data.result.n + " records inserted!");
-    process.exit(0);
+    console.log(data)
+    let workoutSeed = [
+      {
+        day: new Date().setDate(new Date().getDate()-10),
+        exercises: mongoose.Types.ObjectId(data.ops[0]._id)
+      },
+      {
+        day: new Date().setDate(new Date().getDate()-9),
+        exercises: mongoose.Types.ObjectId(data.ops[1]._id)
+      },
+      {
+        day: new Date().setDate(new Date().getDate()-8),
+        exercises: mongoose.Types.ObjectId(data.ops[2]._id)
+        
+      },
+      {
+        day: new Date().setDate(new Date().getDate()-7),
+        exercises: mongoose.Types.ObjectId(data.ops[3]._id)
+        
+      },
+      {
+        day: new Date().setDate(new Date().getDate()-6),
+        exercises: mongoose.Types.ObjectId(data.ops[4]._id)
+      },
+      {
+        day: new Date().setDate(new Date().getDate()-5),
+        exercises: mongoose.Types.ObjectId(data.ops[5]._id)
+      },
+      {
+        day: new Date().setDate(new Date().getDate()-4),
+        exercises: mongoose.Types.ObjectId(data.ops[6]._id)
+      },
+      {
+        day: new Date().setDate(new Date().getDate()-3),
+        exercises: mongoose.Types.ObjectId(data.ops[7]._id)
+      },
+      {
+        day: new Date().setDate(new Date().getDate()-2),
+        exercises: mongoose.Types.ObjectId(data.ops[8]._id)
+      },
+      {
+        day: new Date().setDate(new Date().getDate()-1), 
+        exercises: mongoose.Types.ObjectId(data.ops[9]._id)
+      }
+    ]
+    db.Workout.deleteMany({})
+    .then(() => db.Workout.collection.insertMany(workoutSeed))
+      .then(data => {
+        console.log(data.result.n + " records inserted!");
+        process.exit(0);
+      })
   })
+  
+    
   .catch(err => {
     console.error(err);
     process.exit(1);
