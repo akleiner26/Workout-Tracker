f5b8e1c3294e510299ba3143ab84a9ba5cec49a3
orangeblack13 <AndrewJKleiner@gmail.com>
2020-07-28 18:56:08 -0400

    notes on virtuals added

--- models/workouts.js
+++ models/workouts.js
@@ -17,3 +17,41 @@ const WorkoutSchema = new Schema({
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
 module.exports = Workout;
+
+
+
+/////////////////////////////////////////////
+//Virtuals for Future Use:
+/////////////////////////////////////////////
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+// const WorkoutSchema = new Schema({
+//     day: {
+//         type: Date,
+//         default: Date.now
+//     },
+//     exercises: [
+//         {
+//             type: Schema.Types.ObjectId,
+//             ref: "Exercise"
+//         }
+//     ]
+// },
+//     {
+//         toJSON: {
+//             virtuals: true
+//         }
+//     });
+// // https://mongoosejs.com/docs/populate.html#populate-middleware
+// WorkoutSchema.pre('find', function () {
+//     this.populate('exercises');
+// });
+// // Creates a virtual property `totalDuration`
+// // https://mongoosejs.com/docs/tutorials/virtuals.html#your-first-virtual
+// WorkoutSchema.virtual('totalDuration').get(function () {
+//     return this.exercises.reduce((total, exercise) => {
+//         return total + exercise.duration;
+//     }, 0);
+// });
+// const Workout = mongoose.model("Workout", WorkoutSchema);
+// module.exports = Workout;
