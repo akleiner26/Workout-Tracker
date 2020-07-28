01f4b5560b7264b43c0e19b29977a0274d36e688
orangeblack13 <AndrewJKleiner@gmail.com>
2020-07-28 18:57:00 -0400

    serverjs cleaned of comments and formatting

--- server.js
+++ server.js
@@ -33,73 +33,63 @@ app.get("/stats", (req, res) => {
 });
 
 //Adds to an existing workout
-app.put("/api/workouts/:id", ({body, params}, res) => {
+app.put("/api/workouts/:id", ({ body, params }, res) => {
     db.Exercise.create(body)
-    .then (({_id}) => 
-        db.Workout.findByIdAndUpdate( params.id, { $push: {exercises: _id } }, {new:true})
-    ).then(dbWorkout => {
-        res.json(dbWorkout);
-    }) .catch (err => {
-        res.json(err);
-    })
+        .then(({ _id }) =>
+            db.Workout.findByIdAndUpdate(params.id, { $push: { exercises: _id } }, { new: true })
+        ).then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        })
 })
 
 //adds a new workout
 app.post("/api/workouts", (req, res) => {
     db.Workout.create({})
-    .then(dbWorkout => {
-        res.json(dbWorkout);
-    })
-    .catch(({message}) => {
-        res.json(message)
-    })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(({ message }) => {
+            res.json(message)
+        })
 })
 
 //Get Last Workout
-app.get("/api/workouts" , (req,res) => {
+app.get("/api/workouts", (req, res) => {
     db.Workout.find({}).populate("exercises")
-    .then(dbWorkout => {
-        let workoutArr = [];
-        for (let i = 0; i < dbWorkout.length; i++) {
-            let totalDuration = 0;
-            for (let j=0; j < dbWorkout[i].exercises.length; j++){
-                totalDuration += dbWorkout[i].exercises[j].duration
+        .then(dbWorkout => {
+            let workoutArr = [];
+            for (let i = 0; i < dbWorkout.length; i++) {
+                let totalDuration = 0;
+                for (let j = 0; j < dbWorkout[i].exercises.length; j++) {
+                    totalDuration += dbWorkout[i].exercises[j].duration
+                }
+                let newWorkout = {
+                    day: dbWorkout[i].day,
+                    exercises: dbWorkout[i].exercises,
+                    totalDuration: totalDuration
+                }
+                console.log(newWorkout);
+                workoutArr.push(newWorkout)
             }
-            let newWorkout = {
-                day: dbWorkout[i].day,
-                exercises: dbWorkout[i].exercises,
-                totalDuration: totalDuration
-            }
-            console.log(newWorkout);
-            workoutArr.push(newWorkout)
-        }
-        res.json(workoutArr);
-    }).catch (err => {
-        res.json(err);
-    });
+            res.json(workoutArr);
+        }).catch(err => {
+            res.json(err);
+        });
 });
 
 //Get for the Stats Page
-app.get("/api/workouts/range", (req,res) => {
+app.get("/api/workouts/range", (req, res) => {
     db.Workout.find({})
-    .limit(7)
-    .populate("exercises")
-    .then(dbWorkout => {
-        res.json(dbWorkout);
-    }).catch(err => {
-        res.json(err);
-    })
+        .limit(7)
+        .populate("exercises")
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        })
 })
-
-//Tally total duration with virtual
-// Workout.virtual("duration").get(function() {
-//     return (totalDuration + this.Exercise.duration)
-// })
-
-// //Post total duration
-// app.get("/api/workouts" , (req,res) => {
-//         res.json(totalDuration);
-//     });
 
 // Start the server
 app.listen(PORT, () => {
